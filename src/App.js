fa78b147647b581c2f693684f18d91a6ecf4c166
Amisha <amisha9565@gmail.com>
2024-09-20 23:53:32 +0530

    imported components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { Toaster } from 'react-hot-toast';
+import CourseForm from './components/CourseForm';
+import CourseList from './components/CourseList';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Toaster />
+
+      <div className="main-container">
+        <div className="main-container">
+          <h1
+            style={{
+              fontSize: "2.5rem",
+              marginBottom: "2rem"
+            }}
+          >
+            My Course list
+          </h1>
+          <CourseForm />
+          <CourseList />
+        </div>
+      </div>
+    </>
   );
 }
 
