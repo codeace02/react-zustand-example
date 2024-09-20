6d60501bff40e21ae7ba54594498885400e3e6d1
Amisha <amisha9565@gmail.com>
2024-09-20 23:14:19 +0530

    store created

---
+++ src/app/zustandStore.js
@@ -0,0 +1,33 @@
+import create from 'zustand';
+
+import { devtools, persist } from 'zustand/middleware'
+
+const zustandStore = (set) => ({
+    courses: [],
+    addCourse: (course) => {
+        set((state) => ({
+            courses: [course, ...state.courses],
+        }))
+    },
+    removeCourse: (courseId) => {
+        set((state) => ({
+            courses: state.courses.filter((c) => c.id !== courseId)
+        }))
+    },
+    toggleCourseStatus: (courseId) => {
+        set((state) => ({
+            courses: state.courses.map((course) => course.id === courseId ? { ...course, completed: !course.completed } : course)
+        }))
+    }
+})
+
+const useZustandStore = create(
+    devtools(
+        persist(zustandStore, {
+            name: "courses",
+        })
+    )
+)
+
+
+export default useZustandStore;
