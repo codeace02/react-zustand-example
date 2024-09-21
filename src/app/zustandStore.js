08d6f247f60a9faa5b73f6e11c5119952fa187e1
Amisha <amisha9565@gmail.com>
2024-09-21 14:50:38 +0530

    get api

--- src/app/zustandStore.js
+++ src/app/zustandStore.js
@@ -1,8 +1,14 @@
 
+import axios from 'axios'
 import { create } from 'zustand'
 import { devtools, persist } from 'zustand/middleware'
 
 const zustandStore = (set) => ({
+
+    data: [],
+    loading: false,
+    error: null,
+
     courses: [],
     addCourse: (course) => {
         set((state) => ({
@@ -18,13 +24,24 @@ const zustandStore = (set) => ({
         set((state) => ({
             courses: state.courses.map((course) => course.id === courseId ? { ...course, completed: !course.completed } : course)
         }))
-    }
+    },
+
+    fetchData: async () => {
+        set({ loading: true, error: null });
+        try {
+            const response = await axios.get('https://jsonplaceholder.typicode.com/posts'); // Sample API
+            set({ data: response.data, loading: false });
+        } catch (error) {
+            set({ error: error.message, loading: false });
+        }
+    },
 })
 
 const useZustandStore = create(
     devtools(
         persist(zustandStore, {
             name: "courses",
+            name:"fetchapi"
         })
     )
 )
